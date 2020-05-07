7ef4bff0cce81d2e8cdc99e0466edf0b36b27d1d
Peter Eck <jpeckiii@gmail.com>
2020-05-07 08:33:09 -0500

    Develop shell of addSub handler

--- api/subscription/addSubscription.js
+++ api/subscription/addSubscription.js
@@ -1,12 +1,48 @@
 'use strict';
 
-module.exports.submit = async (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Serverless is up and running!',
-      input: event,
-    })
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.submit = async (event, _, callback) => {
+  const body = JSON.parse(event.body);
+  const subscriptionName = body.subScriptionName;
+  if (typeof subscriptionName !== 'string') {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\t submit candidate because of a validation error'));
+    return;
   }
-  callback(null, response);
+  submitSubscription(subscriptionInfo(subscriptionName))
+};
+
+const submitSubscription = subscription => {
+  console.log(`Submitting subscription...`)
+  const subscriptionInfo = {
+    TableName: process.env.SUBSCRIPTION_TABLE,
+    Item: subscription
+  }
+  return dynamoDb
+    .put(subscriptionInfo)
+    .promise()
+    .then(res => subscription)
+    .catch(err => {
+      console.warn(err);
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: `Unable to add subscription ${ subscription }`
+        })
+      })
+    })
+}
+
+const subscriptionInfo = (subscriptionName, id) => {
+  const timestamp = new Date().getTime();
+  return {
+    id: uuid.v1(),
+    name: subscriptionName
+  };
 };

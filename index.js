024fa2734b2a646273f28137f53ba0936070bc4c
Peter Eck <jpeckiii@gmail.com>
2020-05-07 08:31:58 -0500

    Working with scrape function

---
+++ index.js
@@ -0,0 +1,7 @@
+const scrape = require("./scrape.js");
+
+// console.log("Football Games: ");
+// scrape("https://www.iwusports.com/sports/football/schedule");
+
+console.log("Basketball Games: ");
+scrape("https://www.iwusports.com/sports/womens-basketball/schedule");

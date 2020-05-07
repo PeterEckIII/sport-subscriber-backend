2006fac2ec836f6505550a98c10b911f7c4f10ff
Peter Eck <jpeckiii@gmail.com>
2020-05-07 08:33:28 -0500

    Add serverless-bundle

--- index.mjs
+++
@@ -1,7 +0,0 @@
-import scrape from "./scrape.mjs";
-
-// console.log("Football Games: ");
-// scrape("https://www.iwusports.com/sports/football/schedule");
-
-console.log("Basketball Games: ");
-scrape("https://www.iwusports.com/sports/womens-basketball/schedule");

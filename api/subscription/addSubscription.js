ec19e6f4ec7272a484b35f2c3f7533c608d09af9
Peter Eck <jpeckiii@gmail.com>
2020-05-10 19:11:00 -0500

    Add imports

--- api/subscription/addSubscription.js
+++ api/subscription/addSubscription.js
@@ -1,48 +1,53 @@
-'use strict';
-
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
 const uuid = require('uuid');
 const AWS = require('aws-sdk');
 
 AWS.config.setPromisesDependency(require('bluebird'));
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+app.use(bodyParser.json({ strict: false }));
 
-module.exports.submit = async (event, _, callback) => {
-  const body = JSON.parse(event.body);
-  const subscriptionName = body.subScriptionName;
-  if (typeof subscriptionName !== 'string') {
+app.post('/users', (req, res) => {
+  const { username, email, password } = req.body;
+  if (typeof username !== 'string' || typeof email !== 'string' || typeof password !== 'string') {
     console.error('Validation Failed');
-    callback(new Error('Couldn\t submit candidate because of a validation error'));
-    return;
-  }
-  submitSubscription(subscriptionInfo(subscriptionName))
-};
-
-const submitSubscription = subscription => {
-  console.log(`Submitting subscription...`)
-  const subscriptionInfo = {
-    TableName: process.env.SUBSCRIPTION_TABLE,
-    Item: subscription
-  }
-  return dynamoDb
-    .put(subscriptionInfo)
-    .promise()
-    .then(res => subscription)
-    .catch(err => {
-      console.warn(err);
-      callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: `Unable to add subscription ${ subscription }`
-        })
-      })
-    })
-}
-
-const subscriptionInfo = (subscriptionName, id) => {
-  const timestamp = new Date().getTime();
-  return {
-    id: uuid.v1(),
-    name: subscriptionName
+    res.status(400).json(
+      {
+        error: 'Couldn\'t submit candidate because of a validation error'
+      });
   };
-};
+
+  const timestamp = new Date().getTime();
+  const newUser = {
+    id: uuid.v1(),
+    username: username,
+    email: email,
+    hash: password,
+    createdAt: timestamp,
+    updatedAt: timestamp
+  };
+  const newUserInfo = {
+    TableName: process.env.USER_TABLE,
+    Item: newUser
+  };
+  dynamoDb.put(newUserInfo)
+    .promise()
+    .then(data => {
+      res.status(201).json({
+        message: 'Success adding user',
+        email: data.email,
+        id: data.id,
+        username: data.username
+      });
+    })
+    .catch(err => {
+      console.log(`Error adding user with Error: ${ err }`);
+      res.status(400).json({
+        message: 'Could not create user',
+        error: err
+      });
+    });
+});

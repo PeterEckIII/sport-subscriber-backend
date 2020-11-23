4f1f8022d377d8ac855f025832cab0485bba9724
Peter Eck <jpeckiii@gmail.com>
2020-11-23 06:27:50 -0600

    fix(users-service) -- Change update function to replace entire item

--- users-service/update.js
+++ users-service/update.js
@@ -26,7 +26,6 @@ app.put('/users/:id', async (req, res) => {
     const tableName = process.env.USER_TABLE;
     const id = req.params.id;
 
-    // SET FETCH PARAMS
     const fetchParams = {
         TableName: tableName,
         Key: {
@@ -34,10 +33,11 @@ app.put('/users/:id', async (req, res) => {
         },
     };
     try {
-        const res = await dynamoDb.get(fetchParams).promise();
-        let originalCreatedAt = res.Item.createdAt;
+        const result = await dynamoDb.get(fetchParams).promise();
+        let originalCreatedAt = result.Item.createdAt;
 
         let updatedUser = {
+            id: req.params.id,
             createdAt: originalCreatedAt,
             updatedAt: timestamp,
         };
@@ -52,110 +52,23 @@ app.put('/users/:id', async (req, res) => {
                 id: req.params.id
             },
             Item: updatedUser,
-            ReturnValues: 'ALL_NEW'
         };
-        const updateRes = await dynamoDb.put(updateParams);
-        console.log(updateRes.response.httpResponse.statusCode);
-        console.log(updateRes);
+        try {
+            const updateRes = await dynamoDb.put(updateParams).promise();
+            console.log(updateRes);
+            res.status(201).json({
+                message: 'Success updating user',
+            });
+        } catch (e) {
+            console.log(`Error putting item: ${e}`);
+            res.status(400).json({
+                message: `Error putting item`,
+                error: `${e}`
+            });
+        }
     } catch (e) {
         console.log(`Error fetching user: ${e}`);
     }
-    // dynamoDb
-    //     .get(fetchParams)
-    //     .promise()
-    //     .then(res => {
-    //         return res.Item;
-    //     })
-    //     .then(user => {
-    //         originalCreatedAt = String(user.createdAt);
-    //         console.log(`Stringified created date: ${originalCreatedAt}`);
-    //         return originalCreatedAt;
-    //     })
-    //     .catch(e => console.log(`Error fetching user: ${e}`));
-
-    // const updateParams = {
-    //     TableName: process.env.USER_TABLE,
-    //     Key: {
-    //         "id": req.params.id
-    //     },
-    //     Item: updatedUser
-    // };
 });
 
 exports.handler = serverless(app);
-
-/*eslint no-trailing-spaces: ["error", { "ignoreComments": true }]*/
-
-//     // ====================== OLD CODE ====================== 
-//     const extractUpdateValues = body => {
-//         let itemsToUpdate = {};
-//         for (const param in body) {
-//             console.log(`${ JSON.stringify(param) }: ${ JSON.stringify(body[ param ]) } (Type: ${ typeof param })`);
-//             itemsToUpdate[ param ] = body[ param ];
-//         }
-//         console.log(`Items to update ${ JSON.stringify(itemsToUpdate) }`);
-//         return itemsToUpdate;
-//     };
-
-//     const fieldsToUpdate = extractUpdateValues(body);
-
-//     const timestamp = new Date().getTime();
-
-//     const generateUpdateQuery = fields => {
-//         let updateParams = {
-//             UpdateExpression: 'set updatedAt = :updatedAt',
-//             ExpressionAttributeValues: {
-//                 ':updatedAt': timestamp,
-//             },
-//             ExpressionAttributeNames: {
-//             }
-//         };
-//         Object.entries(fields).forEach(([ key, item ]) => {
-//             if (key === 'subscriptions') {
-//                 updateParams.Key['subscriptions'];
-//                 updateParams.ExpressionAttributeNames[ '#subs' ] = key;
-//                 updateParams.ExpressionAttributeValues[ ':subscriptions' ] = item;
-//                 updateParams.ExpressionAttributeValues[ ':empty_list' ] = [];
-//                 updateParams.UpdateExpression += ` #subs = list_append(if_not_exists(#subs, :empty_list), :subscriptions)`;
-//             } else {
-//                 updateParams.ExpressionAttributeNames[ `#${ key }` ] = key;
-//                 updateParams.ExpressionAttributeValues[ `:${ key }` ] = item;
-//                 updateParams.UpdateExpression += ` #${ key } = :${ key },`;
-//             }
-//         });
-//         updateParams.UpdateExpression = updateParams.UpdateExpression.slice(0, -1);
-//         console.log(`DynamoDB Update Parameters: ${ JSON.stringify(updateParams) }`);
-//         return updateParams;
-//     };
-
-//     const updateFunction = (params) => {
-//         dynamoDb
-//             .update(params)
-//             .promise()
-//             .then(result => {
-//                 res.status(201).json({
-//                     message: `Success updating record`,
-//                 });
-//             })
-//             .catch(err => {
-//                 console.log(`Error adding user with Error: ${ err }`);
-//                 res.status(401).json({
-//                     message: `Error updating user`,
-//                     error: err
-//                 });
-//             });
-//     };
-
-//     const addedParams = generateUpdateQuery(fieldsToUpdate);
-
-//     const baseParams = {
-//         TableName: process.env.USER_TABLE,
-//         Key: {
-//             "id": req.params.id
-//         },
-//         ...addedParams,
-//         ReturnValues: 'ALL_NEW',
-//     };
-
-//     updateFunction(baseParams);
-// });
